93831176b03996f0230c93fb86b4f7a0f5f3507d
Mohamed Mameche <usmalgerie@live.fr>
2023-01-16 23:38:46 +0100

    first version

---
+++ Controllers/CinemasController.cs
@@ -0,0 +1,19 @@
+﻿using eMediaStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eMediaStore.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var cinemasdata = _context.Cinemas.ToList();
+            return View();
+        }
+    }
+}
---
+++ Controllers/MoviesControllers.cs
@@ -0,0 +1,19 @@
+﻿using eMediaStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eMediaStore.Controllers
+{
+    public class MoviesControllers : Controller
+    {
+        private readonly AppDbContext _context;
+        public MoviesControllers(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var moviessdata = _context.Movies.ToList();
+            return View();
+        }
+    }
+}
