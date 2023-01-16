180671cd098720cf9c3335447a4ebb8b36478e35
Mohamed Mameche <usmalgerie@live.fr>
2023-01-16 23:35:44 +0100

    producers controller - first version -

--- Controllers/ProducersController.cs
+++ Controllers/ProducersController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eMediaStore.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eMediaStore.Controllers
 {
     public class ProducersController : Controller
     {
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var producersdata = _context.Producers.ToList();
             return View();
         }
     }
