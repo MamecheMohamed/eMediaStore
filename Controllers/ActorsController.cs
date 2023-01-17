6e37a56aebef323aa079014819bc185caf731438
Mohamed Mameche <usmalgerie@live.fr>
2023-01-17 17:15:50 +0100

    starting Actor View

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -13,7 +13,7 @@ namespace eMediaStore.Controllers
         public IActionResult Index()
         {
             var data=_context.Actors.ToList();
-            return View();
+            return View(data);
         }
     }
 }
--- Models/Actor.cs
+++ Models/Actor.cs
@@ -6,9 +6,16 @@ namespace eMediaStore.Models
     {
         [Key]
         public int ActorId { get; set; }
+
+        [Display(Name ="Profile Picture URL")]
         public string ProfilePicUrl { get; set; }
+
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+
+        [Display(Name = "Biography")]
         public string Biography { get; set; }
+
         public List<Actors_Movies> Actors_Movies { get; set; }
 
 
