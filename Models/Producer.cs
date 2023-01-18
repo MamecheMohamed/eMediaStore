3664ceafba026cb5fe51641521b91eba466059ca
Mohamed Mameche <usmalgerie@live.fr>
2023-01-18 15:50:21 +0100

    ...

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -6,8 +6,13 @@ namespace eMediaStore.Models
     {
         [Key]
         public int ProducerId { get; set; }
+        [Display(Name = "Profile Picture")]
         public string ProfilePicUrl { get; set; }
+
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+
+        [Display(Name = "Biography")]
         public string Biography { get; set; }
         public List<Movie> Movies { get; set; }
     }
