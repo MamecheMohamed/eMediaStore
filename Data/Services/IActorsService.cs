61f663dbb9d6e1c28cba291609af93d2978e36a4
Mohamed Mameche <usmalgerie@live.fr>
2023-01-20 18:28:13 +0100

    Actor Services Interface

---
+++ Data/Services/IActorsService.cs
@@ -0,0 +1,15 @@
+﻿using eMediaStore.Models;
+
+namespace eMediaStore.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> getAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        void Delete(Actor actor);
+        Actor Update(int id, Actor newactor);
+
+
+    }
+}
