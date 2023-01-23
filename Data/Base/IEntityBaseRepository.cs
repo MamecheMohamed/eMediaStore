4e35cc7bde98a25fa5045bca6577b520e050b36e
Mohamed Mameche <usmalgerie@live.fr>
2023-01-23 21:52:56 +0100

    using generic class

---
+++ Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using eMediaStore.Models;
+
+namespace eMediaStore.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T: class , IEntityBase,new()
+    {
+        Task<IEnumerable<T>> getAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(int id, T entity);
+    }
+}
