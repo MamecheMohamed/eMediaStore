2f209ee2054c39e57093dce1d3af191811d3f3af
Mohamed Mameche <usmalgerie@live.fr>
2023-01-26 23:52:15 +0100

    ...

---
+++ Data/Static/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace eMediaStore.Data.Static
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using eMediaStore.Data;
 using eMediaStore.Data.Services;
+using eMediaStore.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,15 @@ builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IMoviesService, MoviesService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
 builder.Services.AddScoped<ICinemasService, CinemasService>();
+
+//Authentication and authorization
+
+/*builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(options =>{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});*/
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
