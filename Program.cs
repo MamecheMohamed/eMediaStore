11e3c7451610b5e21e27eb84583e34985671a9fc
Mohamed Mameche <usmalgerie@live.fr>
2023-02-04 15:49:13 +0100

    adding comments in program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
-
+using static System.Formats.Asn1.AsnWriter;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,20 +18,25 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddEntityFrameworkStores<AppDbContext>();
 
 builder.Services.AddTransient<IEmailSender, EmailSender>();
+//Transient lifetime services are created each time they are requested. This lifetime works best for lightweight, stateless services.
 builder.Services.AddScoped<IActorsService, ActorsService>();
 builder.Services.AddScoped<IMoviesService, MoviesService>();
 builder.Services.AddScoped<IProducersService, ProducersService>();
 builder.Services.AddScoped<ICinemasService, CinemasService>();
+//Scoped lifetime services are created once per request.
 
-//Authentication and authorization
+//builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//Singleton lifetime services are created the first time they are requested
+//(or when ConfigureServices is run if you specify an instance there) and then every subsequent request will use the same instance.
 
-/*builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
-builder.Services.AddMemoryCache();
-builder.Services.AddSession();
-builder.Services.AddAuthentication(options =>{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-});*/
+builder.Services.AddDistributedMemoryCache();
 
+builder.Services.AddSession(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+});
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
@@ -53,6 +58,7 @@ app.UseRouting();
 app.UseAuthentication();;
 
 app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
